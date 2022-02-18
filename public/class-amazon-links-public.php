5fea6c410321e1a6304927d7b920400086a794a8
Youcef Bell <fecuoy03@gmail.com>
2022-02-18 16:21:25 +0100

    creating amazon links cpt / setting up cron job

--- public/class-amazon-links-public.php
+++ public/class-amazon-links-public.php
@@ -1,27 +1,26 @@
 <?php
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * @link       https://codeable.io/developers/sofiane-achouba
  * @since      1.0.0
  *
  * @package    Amazon_Links
- * @subpackage Amazon_Links/admin
+ * @subpackage Amazon_Links/public
  */
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
+ * enqueue the public-facing stylesheet and JavaScript.
  *
  * @package    Amazon_Links
- * @subpackage Amazon_Links/admin
+ * @subpackage Amazon_Links/public
  * @author     Sofiane Achouba < >
  */
-class Amazon_Links_Admin
-{
+class Amazon_Links_Public {
 
 	/**
 	 * The ID of this plugin.
@@ -45,23 +44,22 @@ class Amazon_Links_Admin
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $plugin_name       The name of the plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct($plugin_name, $version)
-	{
+	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
+	 * Register the stylesheets for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles()
-	{
+	public function enqueue_styles() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -75,16 +73,16 @@ class Amazon_Links_Admin
 		 * class.
 		 */
 
-		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/amazon-links-admin.css', array(), $this->version, 'all');
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/amazon-links-public.css', array(), $this->version, 'all' );
+
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Register the JavaScript for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts()
-	{
+	public function enqueue_scripts() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -98,39 +96,8 @@ class Amazon_Links_Admin
 		 * class.
 		 */
 
-		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/amazon-links-admin.js', array('jquery'), $this->version, false);
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/amazon-links-public.js', array( 'jquery' ), $this->version, false );
+
 	}
 
-	/**
-	 * cron function.
-	 *
-	 * @since    1.0.0
-	 */
-	function custom_cron_hook_func()
-	{
-		// code de la fonction a executer dans la tache cron
-		$args = ["post_title" => "post  cron "];
-		wp_insert_post($args);
-	}
-
-	/**
-	 * Amazon links CPT  init.
-	 *
-	 * @since    1.0.0
-	 */
-	public function amazon_link_cpt()
-	{
-		register_post_type('amazon_link', array(
-			'public' => true,
-			'menu_position' => 65,
-			'menu_icon' => 'dashicons-tagcloud',
-			'labels' => array(
-				'name' => __('Amazon Links', 'amazon-links'),
-				'singular_name ' => __('Amazon Link', 'amazon-links'),
-				'menu_name' => __('Amazon Links', 'amazon-links'),
-			)
-		));
-		remove_post_type_support("amazon_link", 'editor');
-		remove_post_type_support("amazon_link", 'thumbnail');
-	}
 }
