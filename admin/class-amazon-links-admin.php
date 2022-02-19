5a3fe57ce4cbf0489a520e0cd270b5d6ef2433ac
Youcef Bell <fecuoy03@gmail.com>
2022-02-20 00:50:03 +0100

    cron job function/custom link column

--- admin/class-amazon-links-admin.php
+++ admin/class-amazon-links-admin.php
@@ -108,8 +108,71 @@ class Amazon_Links_Admin
 	 */
 	function amazon_links_cron_hook_func()
 	{
-		// code de la fonction a executer dans la tache cron
+		global $wpdb;
+		//delete all old ones 
+		$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'amazon_link'");
+		$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
 
+		// check the links in all posts
+		$all_posts = get_posts([
+			"post_type" => "post",
+			'post_status' => 'publish',
+			'posts_per_page' => -1,
+			'order_by' => 'ID',
+			'order' => 'ASC',
+		]);
+		//function to get all links from content
+		function get_urls($string)
+		{
+			$regex = '/https:\/\/(www\.)*am[za].*?(?=[?\'\"])/i';
+			preg_match_all($regex, $string, $matches);
+			return ($matches[0]);
+		}
+		//function to get all links with short codes
+		function get_link_by_short_code($string)
+		{
+			$regex = '/[^\]]+(?=\[\/asa2])/i';
+			$links = array();
+			preg_match_all(
+				$regex,
+				$string,
+				$matches
+			);
+			foreach ($matches[0] as $short_code) {
+				$links[] = "https://www.amazon.es/dp/" . $short_code;
+			}
+			return ($links);
+		}
+		//looping through posts
+		foreach ($all_posts as $post) {
+
+			$final_links = array();
+			$content_links = get_urls($post->post_content);
+			$shortcode_links = get_link_by_short_code($post->post_content);
+			$mypost = [
+				'post_title'    => $post->post_title,
+				'post_type'  => 'amazon_link',
+				'post_status'   => 'publish',
+			];
+
+			//checking for only amazon links
+			foreach ($content_links as $link) {
+
+				$final_links[] = $link;
+			}
+			//checking for shortcode
+			foreach ($shortcode_links as $link) {
+
+				$final_links[] = $link;
+			}
+
+
+			//creating for each amazon link a post
+			foreach (array_unique($final_links) as $final_link) {
+				$id = wp_insert_post($mypost);
+				update_post_meta($id, "the_amazon_link", $final_link);
+			}
+		}
 	}
 
 	/**
@@ -145,19 +208,35 @@ class Amazon_Links_Admin
 	}
 
 	/**
-	 * add custom colun  content box id (header)
+	 * add custom link column  amazon links 
 	 *
 	 * @since    1.0.0
 	 */
-	public function add_custom_column($columns)
+	public function add_link_column($columns)
 	{
 
 
 		$arr = array(
-			'cb' => $columns['cb'],
 			"title" => __('Title'),
-			"the_amazon_link" => __('Link')
+			"the_amazon_link" => __('Link'),
 		);
 		return $arr;
+	}
+
+	/**
+	 * customize link column  (amazon links)
+	 * @since    1.0.0
+	 */
+	public function link_column_data($columns, $post_id)
+	{
+
+		if ($columns == "the_amazon_link") {
+			$links = get_post_meta($post_id, "the_amazon_link", true);
+?>
+
+			<h3><?php echo $links; ?></h3>
+
+<?php
+		}
 	}
 }
